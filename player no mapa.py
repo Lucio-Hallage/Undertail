8487a7c61aad79877a35640ba7dd0034eadcc38a
VitorMiada <49501307+VitorMiada@users.noreply.github.com>
2019-05-15 17:56:29 -0300

    Create player no mapa.py

---
+++ player no mapa.py
@@ -0,0 +1,38 @@
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500,500))
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 30
+
+run = True
+while run:
+    pygame.time.delay(50)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT]:
+        x-= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+    win.fill((0,0,0))
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    pygame.display.update()       
+            
+            
+            
+            
+pygame.quit()
+
+#ainda sem o background e a skin do char
